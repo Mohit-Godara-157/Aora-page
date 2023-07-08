8a629bc1c2829ec8f9ca1146724f68caf747bbf6
Narender-Chahal <nc7206377341@gmail.com>
2023-07-08 11:38:46 +0530

    img add

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-<<<<<<< HEAD
 import HeroSection from "./Components/HeroSection";
-=======
-import Maps from "./Components/Maps";
->>>>>>> 09c9ca4e8e0a25c7e67ee376c69896f4aee0d710
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <HeroSection />
-=======
-      <Maps />
->>>>>>> 09c9ca4e8e0a25c7e67ee376c69896f4aee0d710
     </>
   );
 }
--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import search_icon from "../assets/img/svg/se"
 function HeroSection() {
-  return <section className=" bg-black  min-h-screen"></section>;
+  return (
+    <section className=" bg-[#FFB219] pb-8">
+      <div className="container xl:max-w-[1140px] 2xl:max-w-[1320px] mx-auto">
+        <h2 className="ff_Bariol font-bold text-[32px] text-center leading-8">
+          Hello july
+        </h2>
+        <h1 className="ff_Bariol font-bold text-[48px] text-center leading-[63px]">
+          What services are you looking for?
+        </h1>
+        <div className=" max-w-[1034px] mx-auto bg-white">
+          <input
+            className=" w-full outline-none ff_Bariol font-normal text-base leading-5  text-[#6A6A6A]"
+            type="text"
+            placeholder="Edge: “Beyond Repara” and “Installer Horn”"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
